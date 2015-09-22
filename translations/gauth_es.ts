33bc8fb29a1e892f4d7ae518640a22fa2979d76c
Danko Miocevic <dankomiocevic@gmail.com>
2015-09-22 13:26:03 -0300

    Add spanish translation.

---
+++ translations/gauth_es.ts
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>ApplicationUI</name>
+    <message>
+        <location filename="../src/applicationui.cpp" line="231"/>
+        <source>Copied code: %1 to clipboard</source>
+        <translation>C√digo copiado: %1 al portapapeles</translation>
+    </message>
+</context>
+<context>
+    <name>NewEntry</name>
+    <message>
+        <location filename="../assets/NewEntry.qml" line="7"/>
+        <source>Create</source>
+        <translation>Crear</translation>
+    </message>
+    <message>
+        <location filename="../assets/NewEntry.qml" line="14"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../assets/NewEntry.qml" line="19"/>
+        <source>New Key Entry</source>
+        <translation>Nueva Clave</translation>
+    </message>
+    <message>
+        <location filename="../assets/NewEntry.qml" line="46"/>
+        <source>Enter your secret key from the 2-step verification setup page.</source>
+        <translation>Ingrese su clave secreta de la p√°gina deverificac√≥n de 2 pasos.</translation>
+    </message>
+    <message>
+        <location filename="../assets/NewEntry.qml" line="59"/>
+        <source>Your Account&apos;s name</source>
+        <translation>Nombre de la cuenta</translation>
+    </message>
+    <message>
+        <location filename="../assets/NewEntry.qml" line="65"/>
+        <source>Your Key</source>
+        <translation>Su Clave</translation>
+    </message>
+    <message>
+        <location filename="../assets/NewEntry.qml" line="74"/>
+        <source>Key Length:</source>
+        <translation>Largo de la Clave:</translation>
+    </message>
+    <message>
+        <location filename="../assets/NewEntry.qml" line="104"/>
+        <source>Type:</source>
+        <translation>Tipo:</translation>
+    </message>
+    <message>
+        <location filename="../assets/NewEntry.qml" line="108"/>
+        <location filename="../assets/NewEntry.qml" line="109"/>
+        <source>Time based OTP</source>
+        <translation>Time based OTP</translation>
+    </message>
+    <message>
+        <location filename="../assets/NewEntry.qml" line="113"/>
+        <location filename="../assets/NewEntry.qml" line="114"/>
+        <source>Counter based OTP</source>
+        <translation>Counter based OTP</translation>
+    </message>
+</context>
+<context>
+    <name>QRScanner</name>
+    <message>
+        <location filename="../assets/QRScanner.qml" line="9"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../assets/QRScanner.qml" line="15"/>
+        <source>Scan QR Code</source>
+        <translation>Scanear C√digo QR </translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="9"/>
+        <source>Add account</source>
+        <translation>Agregar cuenta</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="25"/>
+        <source>Scan QR Barcode</source>
+        <translation>Scanear C√digo QR</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="36"/>
+        <source>Share</source>
+        <translation>Compartir</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="137"/>
+        <source>Authenticator Code</source>
+        <translation>C√digo de autentificaci√n</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="140"/>
+        <source>Delete This Entry</source>
+        <translation>Eliminar este valor</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="155"/>
+        <location filename="../assets/main.qml" line="156"/>
+        <source>Confirm Deletion?</source>
+        <translation>Confirmar Borrado?</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="177"/>
+        <source>Generate verification codes for the 2-step verification feature for your Google Account</source>
+        <translation>Generar c√digos de verificaci√n para la verificaci√n de 2 pasos de la cuenta de Google.</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="46"/>
+        <source>Buy me a beer</source>
+        <translation>Comprarme una cerveza</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="41"/>
+        <source>Check out 2 Steps Authenticator in BlackBerry World! You can find it at https://appworld.blackberry.com/webstore/content/29401059/</source>
+        <translation>Encuentra el Autenticador de 2 pasos en BlackBerry World! Puedes encontrarlo en https://appworld.blackberry.com/webstore/content/29401059/</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="78"/>
+        <source>2 Steps Authenticator</source>
+        <translation>Authenticador de 2 pasos</translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="142"/>
+        <source>Are you sure to delete account: %0?</source>
+        <translation>Est√ seguro que quiere borrar la cuenta: %0?</translation>
+    </message>
+</context>
+</TS>
