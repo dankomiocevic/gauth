6bd0eecea1cedd466fd2282d28e713af7454a849
Danko Miocevic <dankomiocevic@users.noreply.github.com>
2015-09-22 13:29:19 -0300

    Correct special characters.

--- translations/gauth_es.ts
+++ translations/gauth_es.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/applicationui.cpp" line="231"/>
         <source>Copied code: %1 to clipboard</source>
-        <translation>C√digo copiado: %1 al portapapeles</translation>
+        <translation>C√≥digo copiado: %1 al portapapeles</translation>
     </message>
 </context>
 <context>
@@ -29,7 +29,7 @@
     <message>
         <location filename="../assets/NewEntry.qml" line="46"/>
         <source>Enter your secret key from the 2-step verification setup page.</source>
-        <translation>Ingrese su clave secreta de la p√°gina deverificac√≥n de 2 pasos.</translation>
+        <translation>Ingrese su clave secreta de la p√°gina de verificaci√≥n de 2 pasos.</translation>
     </message>
     <message>
         <location filename="../assets/NewEntry.qml" line="59"/>
@@ -74,7 +74,7 @@
     <message>
         <location filename="../assets/QRScanner.qml" line="15"/>
         <source>Scan QR Code</source>
-        <translation>Scanear C√digo QR </translation>
+        <translation>Scanear C√≥digo QR </translation>
     </message>
 </context>
 <context>
@@ -87,7 +87,7 @@
     <message>
         <location filename="../assets/main.qml" line="25"/>
         <source>Scan QR Barcode</source>
-        <translation>Scanear C√digo QR</translation>
+        <translation>Scanear C√≥digo QR</translation>
     </message>
     <message>
         <location filename="../assets/main.qml" line="36"/>
@@ -97,7 +97,7 @@
     <message>
         <location filename="../assets/main.qml" line="137"/>
         <source>Authenticator Code</source>
-        <translation>C√digo de autentificaci√n</translation>
+        <translation>C√≥digo de autentificaci√≥n</translation>
     </message>
     <message>
         <location filename="../assets/main.qml" line="140"/>
@@ -113,7 +113,7 @@
     <message>
         <location filename="../assets/main.qml" line="177"/>
         <source>Generate verification codes for the 2-step verification feature for your Google Account</source>
-        <translation>Generar c√digos de verificaci√n para la verificaci√n de 2 pasos de la cuenta de Google.</translation>
+        <translation>Generar c√≥digos de verificaci√≥n para la verificaci√≥n de 2 pasos de la cuenta de Google.</translation>
     </message>
     <message>
         <location filename="../assets/main.qml" line="46"/>
@@ -133,7 +133,7 @@
     <message>
         <location filename="../assets/main.qml" line="142"/>
         <source>Are you sure to delete account: %0?</source>
-        <translation>Est√ seguro que quiere borrar la cuenta: %0?</translation>
+        <translation>Est√° seguro que quiere borrar la cuenta: %0?</translation>
     </message>
 </context>
 </TS>
